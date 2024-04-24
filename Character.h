f8ec2297877fa07d4a5f8e9473d9c130f1116540
Zizo-Jo <zihao.zhou01@estudiant.upf.edu>
2024-04-24 11:53:56 +0200

    This head will hold the data structure of the role(enemy or main character)

---
+++ Character.h
@@ -0,0 +1,26 @@
+// Created by zihao zhou on 2024/4/24.
+// This head will hold the data structure of the role(enemy or main character)
+
+#define MAX_NAME 20
+#define MAX_SKILL 4
+
+typedef struct {
+    char name[MAX_NAME];
+    int atk;
+    int hp;
+    int def;
+    int skill_array[MAX_SKILL];
+}Main_character;
+
+typedef struct {
+    char name[MAX_NAME];
+    int atk;
+    int hp;
+    int def;
+}Enemy;
+
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+#endif //CHARACTER_H
+
