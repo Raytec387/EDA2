e528691c80f691e6d8e71157b5d6ff81f6ea6439
Zizo-Jo <zihao.zhou01@estudiant.upf.edu>
2024-04-24 13:13:38 +0200

    This head will hold the data structure of the role(enemy or main character)

---
+++ common.h
@@ -0,0 +1,16 @@
+// Created on 24/04/2024
+
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <string.h>
+
+#define NAME_LENGTH 20
+#define DESCRITPION_LENGTH 100
+#define MAX_SKILL 4
+
+#endif
