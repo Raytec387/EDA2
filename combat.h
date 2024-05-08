f5abb21557d20350026b3c9a890cc723ad6db27b
Rui Lin <raytec387@gmail.com>
2024-05-08 11:09:48 +0200

    Combat start

--- combat.h
+++ combat.h
@@ -1,2 +1,16 @@
 // Created 08/05/2024
 
+#ifndef _COMBAT_H_
+#define _COMBAT_H_
+
+#define MAX_TURNS 10
+
+typedef struct turn_node {
+    
+} turn_node;
+
+typedef struct turn_queue {  
+
+} turn_queue;
+
+#endif _COMBAT_H_
