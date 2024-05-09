9cab2e73694c45443da8b4394829daf4c3c0d4e6
Zizo-Jo <zihao.zhou01@estudiant.upf.edu>
2024-05-09 22:56:42 +0200

    The function that use json file and a special algorithm to realize the skill system.

---
+++ skill.c
@@ -0,0 +1,44 @@
+#include <stdio.h> 
+#include "cJSON.h"
+#include "cJSON.c"
+#define SIZE_OF_BUFFER 1200
+#include <string.h>
+
+int main() { 
+    // open the file 
+    FILE *fp = fopen("skill.json", "r"); 
+    if (fp == NULL) { 
+        printf("Error: Unable to open the file.\n"); 
+        return 1; 
+    } 
+  
+    // read the file contents into a string 
+    char buffer[SIZE_OF_BUFFER]; 
+    int len = fread(buffer, 1, sizeof(buffer), fp); 
+    fclose(fp); 
+  
+    // parse the JSON data 
+    cJSON *json = cJSON_Parse(buffer); 
+    if (json == NULL) { 
+        const char *error_ptr = cJSON_GetErrorPtr(); 
+        if (error_ptr != NULL) { 
+            printf("Error: %s\n", error_ptr); 
+        } 
+        cJSON_Delete(json); 
+        return 1; 
+    } 
+  
+    // access the JSON data 
+    cJSON *description = cJSON_GetObjectItemCaseSensitive(json, "Defence"); 
+    if (cJSON_IsString(description) && (description->valuestring != NULL)) { 
+        printf("Name: %s\n", description->valuestring); 
+        char s[100];
+        strcpy(s,description->valuestring);
+        cJSON *skill= cJSON_GetObjectItemCaseSensitive(json, s); 
+        printf("Name: %d\n", skill->valueint); 
+    } 
+  
+    // delete the JSON object 
+    cJSON_Delete(json); 
+    return 0; 
+}
