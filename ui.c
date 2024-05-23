4d64ac9ee5b15f4adf9aaa2aed945c5acaa88836
Zizo-Jo <zihao.zhou01@estudiant.upf.edu>
2024-05-23 10:16:09 +0200

    Updated Interface

--- ui.c
+++ ui.c
@@ -3,6 +3,7 @@
 // Main Menu
 void displayMenu(){
     printf("===Red Rain===\n");
+    printf("0.-Go back\n");
     printf("1.-Start a new game.\n");
     printf("2.-Load Game\n");
     printf("3.-Save Game\n");
