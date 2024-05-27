98a92c4d872159e628acd2ae492d40e84ef52413
Zizo-Jo <zihao.zhou01@estudiant.upf.edu>
2024-05-27 12:00:43 +0200

    Modified fucntion crete_new_character, remove the unnecessary variable

--- character.c
+++ character.c
@@ -4,7 +4,7 @@
 
 // All function related to character creation is in here
 
-Character* create_new_character(char name[], Skill skill[]){
+Character* create_new_character(char name[]){
     Character *new_character = malloc(sizeof(*new_character));
     strncpy(new_character->name, name, MAX_STR_NAME);
     return new_character;
