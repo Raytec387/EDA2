84fe0403978c4bd97af5ee3c5df61bf713ef1d72
Zizo-Jo <zihao.zhou01@estudiant.upf.edu>
2024-05-29 11:42:48 +0200

    Add comments to functions

--- main.h
+++ main.h
@@ -6,9 +6,9 @@
 #include "game.h"
 
 // Function definitions 
-void run(Game_state *currentState);
-void start_game(Game_state *currentState);
-void load_game(const char *file_name, Game_state *currentState);
-void save_game(const char *file_name, Game_state *currentState);
+void run(Game_state *currentState);                                 // Run the game/menu
+void start_game(Game_state *currentState);                          // Start a new game, start by charpter I
+void load_game(const char *file_name, Game_state *currentState);    // Load game states from save.json
+void save_game(const char *file_name, Game_state *currentState);    // Save game states to save.json
 
 #endif
--- scenario.h
+++ scenario.h
@@ -14,15 +14,18 @@ typedef struct Scenario {
     int adjCount;
 } Scenario;
 
-Scenario* createScenario(int id, const char* name); 
-void addEdge(Scenario* src, Scenario* dest); 
-void showAdjacentScenarios(Scenario* scenario);
- 
-Scenario* getScenarioById(Scenario* scenarios[], int id, int count); 
-void freeScenarios(Scenario* scenarios[], int count); 
-int scenario_txt(char txt[NAME_LENGTH]);
-void scenario_end_txt(char txt[NAME_LENGTH]);
-void graph_initialize(Scenario *scenarios[MAX_SCENARIO]);
+// Graph part, done with adjacenncy list
+Scenario* createScenario(int id, const char* name);                     // Initialize the nodes in the graph
+void addEdge(Scenario* src, Scenario* dest);                            // Add edge
+void showAdjacentScenarios(Scenario* scenario);                         // Show available adjacent scenario
+void graph_initialize(Scenario *scenarios[MAX_SCENARIO]);               // Build edges for each node in the graph
+Scenario* getScenarioById(Scenario* scenarios[], int id, int count);    // Function to get a scenario by its ID
+void freeScenarios(Scenario* scenarios[], int count);                   // Free the momory
+
+// Execute the story of the game from txt file
+int scenario_txt(char txt[NAME_LENGTH]);                                // The story of the game, return number of enemies
+void scenario_end_txt(char txt[NAME_LENGTH]);                           // The story of the game after the combat
+// Call the scenario_txt() and scenario_end_txt() functions, and check if user won/lost combat
 void story_Navi_battleCheck(Scenario *scenarios[MAX_SCENARIO], Game_state *currentState, Scenario* currentScenario);
 
 #endif
